38df939b588b1f4aa93af97db44e506ea46a47e3
tiagoferrari-projects <tiago-projects@outlook.com>
2023-03-30 00:22:59 -0300

    feat(Login): added login functionality

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './index.scss';
+import Header from '../../components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    
-    <div>
-    <h1>Login</h1>
-    <button><Link to={`/home`}></Link>Home</button>
-    </div>
+    <section className='login'>
+      <Header />
+      <form>
+        <h2>Acesse com seu login ou cadastre-se!</h2>
+        <h3>Você pode entrar com o seu CPF</h3>
+        <div className='login__input-wrapper'>
+          <label htmlFor="name">Digite seu CPF:</label>
+          <input type="text" placeholder='Nome Completo' id='name' className='login__input-name' />
+          <label htmlFor="password">Senha:</label>
+          <input type="password" id="password" placeholder='********' />
+        </div>
+        <button type='submit'><Link to={`/home`}>Entrar</Link></button>
+      </form>
+    </section>
   )
 }
 
-export default Login
+export default Login;
